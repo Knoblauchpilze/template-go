5a43b356c628e0b39a1e9243211d146dfa54d093
KnoblauchPilze <prolific-programmer@tuta.com>
2023-02-19 12:50:33 +0100

    feat: Template structure for a go project

---
+++ cmd/cli/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/KnoblauchPilze/template-go/pkg/example"
+
+func main() {
+	example.PrintHelloWorld("cli")
+}
---
+++ pkg/example/printer.go
@@ -0,0 +1,7 @@
+package example
+
+import "fmt"
+
+func PrintHelloWorld(app string) {
+	fmt.Printf("Hello world from %s!\n", app)
+}

73743895e7334b24cd63325a56212fbb63e82172
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-01-13 18:19:47 +0100

    fix: Wrong syntax and path in database migration script

--- scripts/configure-database.sh
+++ scripts/configure-database.sh
@@ -8,13 +8,13 @@ fi
 DATABASE=$1
 
 echo "Modifying database files..."
-sed -i "s/template_service/${DATABASE}/g" database/template/db_user_create.sql"
-sed -i "s/template_service/${DATABASE}/g" database/template/db_user_drop.sql"
+sed -i "s/template_service/${DATABASE}/g" database/template/db_user_create.sql
+sed -i "s/template_service/${DATABASE}/g" database/template/db_user_drop.sql
 
 sed -i "s/template_service/${DATABASE}/g" database/template/db_create.sql
 sed -i "s/template_service/${DATABASE}/g" database/template/db_drop.sql
 
-sed -i "s/template_service/${DATABASE}/g" database/template/1_create_initial_schema.up.sql
+sed -i "s/template_service/${DATABASE}/g" database/template/migrations/1_create_initial_schema.up.sql
 
 sed -i "s/template_service/${DATABASE}/g" database/Makefile
 sed -i "s/template/${DATABASE}/g" database/Makefile
@@ -31,13 +31,13 @@ sed -i "s/template\//${DATABASE}\//g" .github/workflows/build-and-push.yml
 sed -i "s/template database/${DATABASE} database/g" .github/workflows/build-and-push.yml
 
 echo "Updating test files..."
-sed -i "s/template_service/${DATABASE}/h" internal/controller/helper_test.go
+sed -i "s/template_service/${DATABASE}/g" internal/controller/helper_test.go
 
 echo "Consolidating config files..."
-sed -i "s/template_service/${DATABASE}/h" cmd/app/configs/config-prod.yml
-sed -i "s/template_service/${DATABASE}/h" cmd/app/configs/config-template-dev.yml
+sed -i "s/template_service/${DATABASE}/g" cmd/app/configs/config-prod.yml
+sed -i "s/template_service/${DATABASE}/g" cmd/app/configs/config-template-dev.yml
 
-sed -i "s/template_service/${DATABASE}/h" cmd/app/internal/config.go
-sed -i "s/template_service/${DATABASE}/h" cmd/app/internal/config_test.go
+sed -i "s/template_service/${DATABASE}/g" cmd/app/internal/config.go
+sed -i "s/template_service/${DATABASE}/g" cmd/app/internal/config_test.go
 
 echo "All set! Don't forget to update the README and run the configure.sh script!"

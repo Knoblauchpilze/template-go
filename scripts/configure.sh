569bf18a95c0d83fef35f40577b9a91c77eb7b4b
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-01-13 19:12:55 +0100

    fix: Wrong and missing replacements in configuration script

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -14,16 +14,20 @@ sed -i "s/template-go/${PROJECT}/g" go.mod
 
 echo "Rewriting docker config..."
 sed -i "s/template-go/${PROJECT}/g" build/template-go/Dockerfile
-mv build/template-go build/${PROJECT}
+sed -i "s/app/${PROJECT}/g" build/template-go/Dockerfile
 
 echo "Creating main service from template app..."
 sed -i "s/template-go/${PROJECT}/g" cmd/app/main.go
+sed -i "s/cmd\/app/cmd\/${PROJECT}/g" cmd/app/main.go
 sed -i "s/app/${PROJECT}/g" cmd/app/Makefile
+
+echo "Updating folder structure..."
+mv build/template-go build/${PROJECT}
 mv cmd/app cmd/${PROJECT}
 
 echo "Updating build system..."
 sed -i "s/template-go/${PROJECT}/g" Makefile
-sed -i "s/template-frontend/${PROJECT}/g" .github/workflows/build-and-push.yml
+sed -i "s/template-go/${PROJECT}/g" .github/workflows/build-and-push.yml
 
 echo "Updating README"
 sed -i "s/\${placeholder-for-name}/${PROJECT}/g" README.template.md

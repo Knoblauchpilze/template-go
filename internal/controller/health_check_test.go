2b556ae64c937692303eca50442d100f0e5cbc08
Knoblauchpilze <prolific-programmer@tuta.com>
2026-06-15 08:12:40 +0200

    fix: Updated tests

--- internal/controller/health_check_test.go
+++ internal/controller/health_check_test.go
@@ -1,12 +1,12 @@
 package controller
 
 import (
-	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestIT_HealthcheckController(t *testing.T) {
@@ -16,27 +16,26 @@ func TestIT_HealthcheckController(t *testing.T) {
 	ctx, rw := generateTestEchoContextFromRequest(req)
 
 	err := healthcheck(ctx, conn)
+	require.NoError(t, err, "Actual err: %v", err)
 
-	assert.Nil(t, err)
 	assert.Equal(t, http.StatusOK, rw.Code)
 	assert.Equal(t, "\"OK\"\n", rw.Body.String())
 }
 
 func TestIT_HealthcheckController_WhenConnectionClosed_ExpectServiceUnavailable(t *testing.T) {
 	conn := newTestConnection(t)
-	conn.Close(context.Background())
 
 	req := httptest.NewRequest(http.MethodGet, "/healtcheck", nil)
 	ctx, rw := generateTestEchoContextFromRequest(req)
 
 	err := healthcheck(ctx, conn)
+	require.NoError(t, err, "Actual err: %v", err)
 
-	assert.Nil(t, err)
 	assert.Equal(t, http.StatusServiceUnavailable, rw.Code)
 	expectedResponse := `
 	{
-		"Code": 100,
-		"Message": "An unexpected error occurred"
+		"code": 100,
+		"message": "an unexpected error occurred"
 	}`
 	assert.JSONEq(t, expectedResponse, rw.Body.String())
 }
